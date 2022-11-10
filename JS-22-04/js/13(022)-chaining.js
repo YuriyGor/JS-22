03f50ed7ca1f89f5611d0ce42e877fa2699c9dff
YuriyGor <110548137+YuriyGor@users.noreply.github.com>
2022-11-10 12:14:27 +0200

    add 13(022)-chaining.js

---
+++ JS-22-04/js/13(022)-chaining.js
@@ -0,0 +1,59 @@
+
+// Цепочки вызовов методов chaining
+
+
+const numbers = [1, 5, 2, 4, 3];
+
+const greaterThenTwo = numbers.filter(num => num > 2);
+console.log(greaterThenTwo);
+
+const multByThree = greaterThenTwo.map(num => num + 3);
+console.log(multByThree);
+
+const sorted = multByThree.sort((a, b) => a - b);
+console.log(sorted);
+
+// Цепочка предыдущих трех
+const result = numbers.filter(num => num > 2)
+    .map(num => num * 3)
+    .sort((a, b) => a - b);
+console.log(result);
+
+
+// Сортируем тех кто онлайн по рангу
+//     - сначала фильтруем
+//     - потом сортируем
+
+const players = [
+  { id: `id-1`, tag: `Mango`, isOnline: true, rank: 800 },
+  { id: `id-2`, tag: `Poly`, isOnline: false, rank: 600 },
+  { id: `id-3`, tag: `Ajax`, isOnline: true, rank: 100 },
+  { id: `id-4`, tag: `Kiwi`, isOnline: true, rank: 400 },
+];
+
+const onlineAndSorted = players
+    .filter(player => player.isOnline)
+    .sort((playerA, playerB) => playerA.rank - playerB.rank);
+console.table(onlineAndSorted);
+
+
+
+// Chaining в методах объекта как jquery
+
+const element = {
+    class: ``,
+    hovered: false,
+    changeClass(cls) {
+        this.class = cls;
+
+        return this;
+    },
+    toggleHovered() {
+        this.hovered = !this.hovered;
+
+        return this;
+    },
+};
+
+element.toggleHovered().changeClass(`open`);
+console.log(element);
