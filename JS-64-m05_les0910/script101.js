a69ad4bd549b3c1c918de50d984fd8b224ce7dc6
YuriyGor <110548137+YuriyGor@users.noreply.github.com>
2022-11-23 11:17:27 +0200

    add script101.js

---
+++ JS-64-m05_les0910/script101.js
@@ -0,0 +1,39 @@
+// const a = 10;
+// a = 15; //Викличе помилку
+// let b; //undefined
+// b = 15;
+
+
+// console.log(`a`,a);
+// var a = 15;
+// console.log(`a`, a);
+// var a = 20;
+// console.log(`a`, a);
+// // console.log(`b`,b);
+// // let b = 20
+
+
+
+// let a = 10;
+
+
+// if (true) {
+//     console.log(a);
+//     var b = 15;
+// }
+// // console.log(b);
+
+
+// function foo() {
+//     console.log(`b inside function`, b);
+// }
+// foo()
+
+
+
+const b = `hello`;
+
+
+{
+    
+}
