06cec5781fa17d08ccd045fb88fce550e4a31c87
YuriyGor <110548137+YuriyGor@users.noreply.github.com>
2022-11-30 21:04:19 +0200

    correct JS-64

--- JS-64-m03_les06/js/index.js
+++ JS-64-m03_les06/js/index.js
@@ -26,32 +26,27 @@ function calcBMI({ weight, height }) {
 //   calcBMI({
 //     weight: `118,3`,
 //     height: `1.95`,
-//   })
-
-
+//   }))
 
 /**
- * 2. 
+ * 2.
  * Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
  * незалежних аргументів.
  */
 
- function printContactsInfo({names, phones}) {
-  const nameList = names.split(',');
-  const phoneList = phones.split(',');
-  
+function printContactsInfo({ names, phones }) {
+  const nameList = names.split(",");
+  const phoneList = phones.split(",");
+
   for (let i = 0; i < nameList.length; i += 1) {
     console.log(`${nameList[i]}: ${phoneList[i]}`);
   }
 }
 
-// printContactsInfo({
-//   names: 'Jacob,William,Solomon,Artemis',
-//   phones: '89001234567,89001112233,890055566377,890055566300',
-// });
-
-
-
+printContactsInfo({
+  names: "Jacob,William,Solomon,Artemis",
+  phones: "89001234567,89001112233,890055566377,890055566300",
+});
 
 /**
  * 3. Перепиши функцію так, щоб вона приймала один об'єкт параметрів замість набору
@@ -59,84 +54,70 @@ function calcBMI({ weight, height }) {
  */
 
 function getBotReport(object) {
-  
-  console.log(object)
+  console.log(object);
 
-  const {companyName, bots} = object
+  const { companyName, bots } = object;
   console.log(companyName);
   console.log(bots);
 
-  const {
-    repair: repairBots = 56, 
-    defence: defenceBots = 45
-  } = bots;
+  const { repair: repairBots = 56, defence: defenceBots = 45 } = bots;
 
-  console.log(repairBots)
-  console.log(defenceBots)
-
+  console.log(repairBots);
+  console.log(defenceBots);
 
   return `${companyName} has ${repairBots + defenceBots} bots in stock`;
 }
 
 const company = {
-  companyName: 'Cyberdyne Systems',
+  companyName: "Cyberdyne Systems",
   bots: {
     repair: 150,
     defence: 50,
   },
-}
+};
 
-getBotReport(company)
-
-
-
+getBotReport(company);
 
 /**
- * 4. 
- * Напиши функцію так, щоб вона приймала об'єкт параметрів із 
- * властивостями `companyName` та `stock` та 
+ * 4.
+ * Напиши функцію так, щоб вона приймала об'єкт параметрів із
+ * властивостями `companyName` та `stock` та
  * виводила репорт про кількість товарів на складі будь-якої компанії.
- * 
+ *
  * Extended: Переписали так щоб функція прймала масив з обʼєктами
  */
 
 const companies = [
   {
-    companyName: 'Cyberdyne Systems',
+    companyName: "Cyberdyne Systems",
     stock: {
       repairBots: 150,
       defenceBots: 50,
     },
-  }, 
+  },
   {
-    companyName: 'Belacci',
+    companyName: "Belacci",
     stock: {
       shoes: 20,
       skirts: 10,
       hats: 5,
     },
-  }
-]
+  },
+];
 
 function getStockReport(companies) {
-  
-  for (let {companyName, stock} of companies) {
-    
+  for (let { companyName, stock } of companies) {
     let total = 0;
     for (let value of Object.values(stock)) {
-      total += value
+      total += value;
     }
 
-    console.log(`${companyName} has ${total} items in stock`)
+    console.log(`${companyName} has ${total} items in stock`);
   }
   // return `${companyName} has ${total} items in stock`
-
 }
 
 // getStockReport(companies)
-
-
-
 
 /**
  * Task 5
@@ -145,61 +126,55 @@ function getStockReport(companies) {
  * значенням "default" якщо в `contact` немає такої властивості.
  */
 
-const createContact = function(contact) {
+const createContact = function (contact) {
   // 1. Створити новий обʼєкт
   // 2. Додати до нього нові ключі
   //   id
   //   createdAt
   //   list, якщо його нема, то дати дефолтне значення
   return {
-    list: 'default',
+    list: "default",
     ...contact,
-    id: 1
-  }
-}
+    id: 1,
+  };
+};
 
 const contact = {
-  name: 'Mango',
-  email: 'mango@mail.com',
-  list: 'friends',
-}
+  name: "Mango",
+  email: "mango@mail.com",
+  list: "friends",
+};
 
 const newContact = createContact(contact);
 newContact.name = "Ser. Pan";
 
-console.log(newContact)
-console.log(contact)
-
-
-
+console.log(newContact);
+console.log(contact);
 
 /**
- * 6. 
- * Напиши функцію `transformUsername(user)` так, 
+ * 6.
+ * Напиши функцію `transformUsername(user)` так,
  * щоб вона повертала новий об'єкт із властивістю
  * `fullName`, замість `firstName` та `lastName`.
  */
 
-const transformUsername = function ({firstName, lastName, ...restProps}) {
-
+const transformUsername = function ({ firstName, lastName, ...restProps }) {
   return {
     fullName: `${firstName} ${lastName}`,
-    ...restProps
-  }
-}
+    ...restProps,
+  };
+};
 
 const user = {
   id: 1,
-  firstName: 'Jacob',
-  lastName: 'Mercer',
-  email: 'j.mercer@mail.com',
+  firstName: "Jacob",
+  lastName: "Mercer",
+  email: "j.mercer@mail.com",
   friendCount: 40,
-}
+};
 
 const newUser = transformUsername(user);
-console.log(newUser)
-
-
+console.log(newUser);
 
 // Питання з кахуту:
 
