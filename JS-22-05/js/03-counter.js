f1a5b77b30e073eca73c41bd7f1202d05efb7cb0
YuriyGor <110548137+YuriyGor@users.noreply.github.com>
2022-12-03 17:54:28 +0200

    correct 03 in JS-22-05

--- JS-22-05/js/03-counter.js
+++ JS-22-05/js/03-counter.js
@@ -1,40 +1,40 @@
 
-const counter = {
-    value: 0,
-    increment() {
-        console.log(`increment -> this`, this);
-        this.value += 1;
-    },
-    decrement() {
-        console.log(`decrement -> this`, this);
-        this.value -= 1;
-    },
-};
+// const counter = {
+//     value: 0,
+//     increment() {
+//         console.log(`increment -> this`, this);
+//         this.value += 1;
+//     },
+//     decrement() {
+//         console.log(`decrement -> this`, this);
+//         this.value -= 1;
+//     },
+// };
 
-const decrementBtn = document.querySelector(`.js-decrement`);
-const incrementBtn = document.querySelector(`.js-increment`);
-const valueEl = document.querySelector(`.js-value`);
+// const decrementBtn = document.querySelector(`.js-decrement`);
+// const incrementBtn = document.querySelector(`.js-increment`);
+// const valueEl = document.querySelector(`.js-value`);
 
-// console.log(decrementBtn);
-// console.log(incrementBtn);
-// console.log(valueEl);
+// // console.log(decrementBtn);
+// // console.log(incrementBtn);
+// // console.log(valueEl);
 
-decrementBtn.addEventListener(`click`, function () {
-    console.log(`Кликнули на декремент`);
+// decrementBtn.addEventListener(`click`, function () {
+//     console.log(`Кликнули на декремент`);
 
-    counter.decrement();
-    console.log(counter);
+//     counter.decrement();
+//     console.log(counter);
 
-    valueEl.textContent = counter.value;
-});
+//     valueEl.textContent = counter.value;
+// });
 
-incrementBtn.addEventListener(`click`, function () {
-  console.log(`Кликнули на инкремент`);
+// incrementBtn.addEventListener(`click`, function () {
+//   console.log(`Кликнули на инкремент`);
 
-    counter.increment();
-    console.log(counter);
+//     counter.increment();
+//     console.log(counter);
 
-    valueEl.textContent = counter.value;
-});
+//     valueEl.textContent = counter.value;
+// });
 
 
